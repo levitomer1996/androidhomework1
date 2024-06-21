2db5674ca146742ee17d6d02db01e9fe5f4205df
levit <levitomer1996@gmail.com>
2024-06-21 15:20:49 +0300

    changed system to work on num matrix

--- app/src/main/java/com/example/homework1/TankGameActivity.java
+++ app/src/main/java/com/example/homework1/TankGameActivity.java
@@ -5,23 +5,44 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageView;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class TankGameActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "TankActivity";
+
+    public int getTankPosition() {
+        return tankPosition;
+    }
+
+
+    private int score = 0;
     private int tankPosition = 1;
     public static final int NUM_OF_COLS = 3;
     public static final int NUM_OF_ROWS = 4;
+
+
     private int num_of_lives = 3;
 
-    private int[][] leaner_ids;
+
+    private int[][] type_mat = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
     private int[] lives = {R.id.live0, R.id.live1, R.id.live2};
+
+
+    private int[][] leaner_ids = {{R.id.row0col0, R.id.row0col1, R.id.row0col2},
+            {R.id.row1col0, R.id.row1col1, R.id.row1col2},
+            {R.id.row2col0, R.id.row2col1, R.id.row2col2},
+            {R.id.row3col0, R.id.row3col1, R.id.row3col2}};
+
+    private int[] colisionZone = {0, 0, 0};
     private int[] col0;
     private int[] col1;
     private int[] col2;
@@ -43,12 +64,7 @@ public class TankGameActivity extends AppCompatActivity {
         rightButton = findViewById(R.id.right_button);
 
         // Setting tank component at the center
-        ImageView tankcol_0 = findViewById(R.id.row3col0);
-        tankcol_0.setVisibility(View.INVISIBLE);
 
-        ImageView tankcol_1 = findViewById(R.id.row3col1);
-        ImageView tankcol_2 = findViewById(R.id.row3col2);
-        tankcol_2.setVisibility(View.INVISIBLE);
 
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,60 +85,8 @@ public class TankGameActivity extends AppCompatActivity {
                 }
             }
         });
-        recognizeImageViews();
+        clearImages();
         startTimer();
-    }
-
-    public void setTankPosition(int pos) {
-        this.tankPosition = pos;
-    }
-
-    public void renderTankPos(int pos) {
-        ImageView tankcol_0 = findViewById(R.id.row3col0);
-        ImageView tankcol_1 = findViewById(R.id.row3col1);
-        ImageView tankcol_2 = findViewById(R.id.row3col2);
-
-        switch (pos) {
-            case 0:
-                tankcol_0.setVisibility(View.VISIBLE);
-                tankcol_1.setVisibility(View.INVISIBLE);
-                tankcol_2.setVisibility(View.INVISIBLE);
-                break;
-            case 1:
-                tankcol_0.setVisibility(View.INVISIBLE);
-                tankcol_1.setVisibility(View.VISIBLE);
-                tankcol_2.setVisibility(View.INVISIBLE);
-                break;
-            case 2:
-                tankcol_0.setVisibility(View.INVISIBLE);
-                tankcol_1.setVisibility(View.INVISIBLE);
-                tankcol_2.setVisibility(View.VISIBLE);
-                break;
-        }
-    }
-
-    public int[][] recognizeImageViews() {
-
-        int[][] ids = {
-                {R.id.row0col0, R.id.row0col1, R.id.row0col2},
-                {R.id.row1col0, R.id.row1col1, R.id.row1col2},
-                {R.id.row2col0, R.id.row2col1, R.id.row2col2}
-        };
-
-        this.setLeaner_ids(ids);
-
-        setCol0(ids[0]);
-        setCol1(ids[1]);
-        setCol2(ids[2]);
-        for (int row = 0; row < ids.length; row++) {
-            for (int col = 0; col < ids[row].length; col++) {
-                ImageView imageView = findViewById(ids[row][col]);
-                if (imageView != null) {
-                    imageView.setImageDrawable(null);
-                }
-            }
-        }
-        return ids;
     }
 
     private void startTimer() {
@@ -131,6 +95,7 @@ public class TankGameActivity extends AppCompatActivity {
             public void onTick(long millisUntilFinished) {
                 launchItem();
                 moveOneStepDown();
+                detectCrash();
             }
 
             @Override
@@ -141,79 +106,105 @@ public class TankGameActivity extends AppCompatActivity {
         gameTimer.start(); // Start the timer
     }
 
-    public void launchRocket() {
-        ImageView row0_col0 = findViewById(this.getLeaner_ids()[0][0]);
-        ImageView row0_col1 = findViewById(this.getLeaner_ids()[0][1]);
-        ImageView row0_col2 = findViewById(this.getLeaner_ids()[0][2]);
-        int randCol = getRandomCol();
-        if (randCol == 0) {
-            row0_col0.setImageResource(R.drawable.rpghead);
-        }
-        if (randCol == 1) {
-            row0_col1.setImageResource(R.drawable.rpghead);
-        }
-        if (randCol == 2) {
-            row0_col2.setImageResource(R.drawable.rpghead);
-        }
-    }
-
-    public void launchBamba() {
-        ImageView row0_col0 = findViewById(this.getLeaner_ids()[0][0]);
-        ImageView row0_col1 = findViewById(this.getLeaner_ids()[0][1]);
-        ImageView row0_col2 = findViewById(this.getLeaner_ids()[0][2]);
-        int randCol = getRandomCol();
-        if (randCol == 0) {
-            row0_col0.setImageResource(R.drawable.bamba);
-        }
-        if (randCol == 1) {
-            row0_col1.setImageResource(R.drawable.bamba);
-        }
-        if (randCol == 2) {
-            row0_col2.setImageResource(R.drawable.bamba);
-        }
+    public void setTankPosition(int pos) {
+        this.tankPosition = pos;
     }
 
     public void launchItem() {
         Random random = new Random();
-        int num = random.nextInt(2);
-        switch (num) {
-            case 0:
-                launchRocket();
-            case 1:
-                launchBamba();
+        int randomItem = getRandomNumber(1);
+        int randomCol = getRandomNumber(2);
+
+        switch (randomItem) {
+
+            case 0: {
+                this.setType_mat(0, randomCol, 0);
+                break;
+            }
+
+            case 1: {
+                this.setType_mat(0, randomCol, 1);
+                break;
+            }
+        }
+        Log.i(LOG_TAG, "Type is " + this.getType_mat()[0][randomCol]);
+        logTypeMat();
+        renderItem(0, randomCol);
+    }
+
+    public void renderItem(int r, int c) {
+        ImageView imageView = findViewById(this.getLeaner_ids()[r][c]);
+
+        switch (this.getType_mat()[r][c]) {
+            case -1: {
+                imageView.setImageDrawable(null);
+                break;
+            }
+            case 0: {
+                imageView.setImageResource(R.drawable.rpghead);
+                break;
+            }
+            case 1: {
+                imageView.setImageResource(R.drawable.bamba);
+                break;
+            }
+
         }
     }
 
     public void moveOneStepDown() {
-        ImageView col0;
-        ImageView col1;
-        ImageView col2;
-        for (int i = NUM_OF_ROWS - 1; i >= 0; i--) {
-
-            if (i + 1 != 3) {
-                col0 = findViewById(this.getLeaner_ids()[i][0]);
-                col1 = findViewById(this.getLeaner_ids()[i][1]);
-                col2 = findViewById(this.getLeaner_ids()[i][2]);
-
-                if (col0 != null) {
-                    ImageView col0next = findViewById(this.getLeaner_ids()[i + 1][0]);
-                    col0next.setImageDrawable(col0.getDrawable());
-                    col0.setImageDrawable(null);
+        for (int r = NUM_OF_ROWS - 2; r >= 0; r--) { // Adjusted loop condition
+            for (int c = 0; c < NUM_OF_COLS; c++) {
+                Log.i("TANK", r + " " + c);
+                if (r + 1 != 4) {
+                    this.setType_mat(r + 1, c, this.getType_mat()[r][c]); // Move the value down one row
                 }
-                if (col1 != null) {
-                    ImageView col1next = findViewById(this.getLeaner_ids()[i + 1][1]);
-                    col1next.setImageDrawable(col1.getDrawable());
-                    col1.setImageDrawable(null);
-                }
-                if (col2 != null) {
-                    ImageView col2next = findViewById(this.getLeaner_ids()[i + 1][2]);
-                    col2next.setImageDrawable(col2.getDrawable());
-                    col2.setImageDrawable(null);
+                this.setType_mat(r, c, -1); // Set the current position to -1
+
+                renderItem(r, c);
+                if (r + 1 != 4) {
+                    renderItem(r + 1, c);
+                    renderTankPos(this.getTankPosition());
                 }
             }
         }
+    }
 
+    public void detectCrash() {
+        for (int i = 0; i < NUM_OF_COLS; i++) {
+            if (this.getType_mat()[3][i] == 0 && this.getTankPosition() == i) {
+                removeOneLive();
+                this.num_of_lives--;
+            }
+            if (this.getType_mat()[3][i] == 1 && this.getTankPosition() == i) {
 
+            }
+        }
+    }
+
+    private void logTypeMat() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("type_mat = [\n");
+        for (int i = 0; i < type_mat.length; i++) {
+            sb.append("  {");
+            for (int j = 0; j < type_mat[i].length; j++) {
+                sb.append(this.getType_mat()[i][j]);
+                if (j < type_mat[i].length - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("}");
+            if (i < type_mat.length - 1) {
+                sb.append(",\n");
+            }
+        }
+        sb.append("\n]");
+        Log.d("TankActivity", sb.toString());
+    }
+
+    public static int getRandomNumber(int max) {
+        Random random = new Random();
+        return random.nextInt(max + 1);
     }
 
     private void removeOneLive() {
@@ -225,33 +216,48 @@ public class TankGameActivity extends AppCompatActivity {
         }
     }
 
-    public static int getRandomCol() {
-        Random random = new Random();
-        return random.nextInt(3);  // Generates a random number between 0 (inclusive) and 3 (exclusive)
+    public void renderTankPos(int pos) {
+        ImageView tankcol_0 = findViewById(R.id.row3col0);
+        ImageView tankcol_1 = findViewById(R.id.row3col1);
+        ImageView tankcol_2 = findViewById(R.id.row3col2);
+
+        switch (pos) {
+            case 0:
+                tankcol_0.setImageResource(R.drawable.tank);
+                tankcol_1.setImageDrawable(null);
+                tankcol_2.setImageDrawable(null);
+                break;
+            case 1:
+                tankcol_0.setImageDrawable(null);
+                tankcol_1.setImageResource(R.drawable.tank);
+                tankcol_2.setImageDrawable(null);
+                break;
+            case 2:
+                tankcol_0.setImageDrawable(null);
+                tankcol_1.setImageDrawable(null);
+                tankcol_2.setImageResource(R.drawable.tank);
+                break;
+        }
     }
 
-    public int[] getCol0() {
-        return col0;
+    public void clearImages() {
+        int[][] list = this.getLeaner_ids();
+        for (int row = 0; row < list.length; row++) {
+            for (int col = 0; col < list[row].length; col++) {
+                ImageView imageView = findViewById(list[row][col]);
+                if (imageView != null) {
+                    imageView.setImageDrawable(null);
+                }
+            }
+        }
     }
 
-    public void setCol0(int[] col0) {
-        this.col0 = col0;
+    public int[][] getType_mat() {
+        return type_mat;
     }
 
-    public int[] getCol1() {
-        return col1;
-    }
-
-    public void setCol1(int[] col1) {
-        this.col1 = col1;
-    }
-
-    public int[] getCol2() {
-        return col2;
-    }
-
-    public void setCol2(int[] col2) {
-        this.col2 = col2;
+    public void setType_mat(int r, int c, int val) {
+        type_mat[r][c] = val;
     }
 
     public int[][] getLeaner_ids() {
@@ -260,5 +266,13 @@ public class TankGameActivity extends AppCompatActivity {
 
     public void setLeaner_ids(int[][] leaner_ids) {
         this.leaner_ids = leaner_ids;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
